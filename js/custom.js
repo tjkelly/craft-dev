589fad9b999ecdfe911abbcb79a905720b2b447f
TJ Kelly <tj@bowst.com>
2014-11-12 00:28:28 -0500

    Initial commit
    Adding assets and initial page version

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+/**
+ *  Craft.Digital Javascript
+ *
+ *  @author TJ Kelly - http://www.tjkelly.com
+ *
+ *  @date 2014-11-08
+ *
+ *  @desc Custom javascript & jQuery functions
+ */
+// Call Foundation & wait for DOM ready
+jQuery(document).foundation().ready(function($){
+
+
+// End - wait for DOM ready	
+});
