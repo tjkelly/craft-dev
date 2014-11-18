cfd7782af5e17c08795f499c62e313279564268a
TJ Kelly <tj@bowst.com>
2014-11-17 22:24:59 -0500

    Added interactivity
    Header/nav bar sticks to top of screen on scroll. First panel is 100%
    width/height and content uses jQuery to calculate/set vertical offset.
    Responsiveness updated to accommodate.

--- js/custom.js
+++ js/custom.js
@@ -11,5 +11,39 @@
 jQuery(document).foundation().ready(function($){
 
 
+
+
+	
+	/**
+	 *  Panel 1 Dimensions
+	 *
+	 *  @author TJ Kelly - http://www.tjkelly.com
+	 *
+	 *  @date 2014-11-17
+	 *
+	 *  @desc Read viewport dimensions & define panel 1 dimensions accordingly
+	 */
+	var intViewportHeight = window.screen.availHeight;
+	
+	// Cache selectors
+	var panel_1 = $('section.panel-1'), panel_1_content = $('section.panel-1 article.panel-content'), header_wrap = $('div.header-wrap');
+	
+	// If 641+
+	if($(window).width() >= 641){
+		
+		// Set dimensions
+		panel_1_content.css({
+			'padding-top' : (panel_1.height() / 2 * .8)
+		});
+	
+	// Else - If 600 or less
+	} else {
+		
+		//
+		header_wrap.prependTo(panel_1);
+			
+	// End - If 641+
+	}
+
 // End - wait for DOM ready	
 });
